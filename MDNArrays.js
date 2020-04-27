94033a02346dd2103723c6923c56b06eb136bb36
agbleze <62272559+agbleze@users.noreply.github.com>
2020-04-27 23:33:53 +0200

    Update MDNArrays.js

--- MDNArrays.js
+++ MDNArrays.js
@@ -103,3 +103,14 @@ let itemText = name + '- $' + price;
 console.log(itemText)
 
 }
+
+/*
+Arrays 1
+Let's start off with some basic array practice. In this task we'd like you to 
+create an array of three items, stored inside a variable called myArray. The items 
+can be anything you want — how about your favourite foods or bands?
+
+Next, modify the first two items in the array using simple bracket notation and assignment.
+ Then add a new item to the beginning of the array.
+
+ */
